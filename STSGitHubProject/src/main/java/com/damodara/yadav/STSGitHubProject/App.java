8833633ec5255fa162231cfc3807e28eb720cb59
dcharan <gurucharan.damodara@gmail.com>
2018-08-27 08:55:57 +0530

    First Commit

---
+++ STSGitHubProject/src/main/java/com/damodara/yadav/STSGitHubProject/App.java
@@ -0,0 +1,13 @@
+package com.damodara.yadav.STSGitHubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
